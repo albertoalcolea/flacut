3fb422610919f7fb0c8944651525ae0ca4ce3689
Alberto Alcolea <albertoalcolea@gmail.com>
2012-06-16 21:38:23 +0200

    shell script added

---
+++ flacut.sh
@@ -0,0 +1,36 @@
+#! /bin/sh
+
+USAGE="Usage: $0 FLAC_FILE CUE_FILE"
+
+if [ "$1" = "--help" ] ; then
+    echo $USAGE
+    exit 0
+fi
+
+if [ $# != 2 ] ; then
+    echo $USAGE
+    exit 1
+else
+    FFLAC="$1"
+    FCUE="$2"
+fi
+
+# Cortamos el archivo flac en múltiples tracks según los índices del archivo cue.
+echo
+echo "Dividiendo fichero flack en multiples tracks"
+echo
+cuebreakpoints "$FCUE" | shnsplit -o flac "$FFLAC"
+
+# Aplicamos las etiquetas tag a los archivos generados.
+echo
+echo "Aplicando tags a los tracks"
+echo
+cuetag "$FCUE" split-track*.flac
+
+# Renombramos los arcivos
+echo
+echo "Renombrando tracks segun los tags asociados"
+echo
+lltag --yes --no-tagging --rename '%n - %t' `ls split-track*.flac`
+
+exit 0
