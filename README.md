c7f3e304cc16b63873594b9d0aaec2ffe0a0a565
Alberto Alcolea <albertoalcolea@gmail.com>
2012-06-25 00:31:23 +0200

    added control of dependencies

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ flacut
 ======
 
 Automated system for cutting up and tagging ".flac" files
+
+Dependencies:
+ - cuetools
+ - lltag
--- flacut.sh
+++ flacut.sh
@@ -2,6 +2,8 @@
 
 USAGE="Usage: $0 FLAC_FILE CUE_FILE"
 
+
+# Recogemos los argumentos con los que ha sido llamado
 if [ "$1" = "--help" ] ; then
     echo $USAGE
     exit 0
@@ -14,6 +16,29 @@ else
     FFLAC="$1"
     FCUE="$2"
 fi
+
+
+
+# Comprobamos que existen las dependencias necesarias
+which cuebreakpoints > /dev/null
+if [ ! $(echo $?) = 0 ] ; then
+    echo "You need to install 'cuetools' to run this application"
+    exit 2
+fi
+
+which cuetag > /dev/null
+if [ ! $(echo $?) = 0 ] ; then
+    echo "You need to install 'cuetools' to run this application"
+    exit 2
+fi
+
+which lltag > /dev/null
+if [ ! $(echo $?) = 0 ] ; then
+    echo "You need to install 'lltag' to run this application"
+    exit 2
+fi
+
+
 
 # Cortamos el archivo flac en múltiples tracks según los índices del archivo cue.
 echo
@@ -32,5 +57,4 @@ echo
 echo "Renombrando tracks segun los tags asociados"
 echo
 lltag --yes --no-tagging --rename '%n - %t' `ls split-track*.flac`
-
 exit 0
